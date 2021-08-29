1a834418e302527be8cd7729be1c716cb33a8456
Sergey <myliveweb@mail.ru>
2021-08-29 06:19:27 +0300

    Footer Media Query

--- components/Footer.js
+++ components/Footer.js
@@ -8,12 +8,16 @@ const Footer = () => {
 
 				<div className="col-sm-12">
 
-                    <p className="copyright">
-                        © 2014-2021 Сергей Запольский&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;Skype: myliveweb&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;Почта: info@zapolskiy.moscow&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;Тел: +7 962 206-4142
+          <p className="copyright">
+            <div>© 2014-2021 Сергей Запольский</div>
+            <div className="pipe">|</div>
+            <div>Skype: myliveweb</div>
+            <div className="pipe">|</div>
+            <div>Почта: info@zapolskiy.moscow</div>
+            <div className="pipe">|</div>
+            <div>Тел: +7 962 206-4142</div>
 					</p>
-
 				</div>
-
 			</div>
 		</div>
 	</footer>
